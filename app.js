208bab961744d05f5cca876a7325086863eb087f
souravsingh1903 <souravsingh1903@gmail.com>
2024-01-31 00:38:01 +0530

    express ading 2 middlewares

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const http = require('http');
-const routes = require('./routes');
-const server = http.createServer(routes);
+// const http = require('http');
+// const routes = require('./routes');
+// const server = http.createServer(routes);
 
-server.listen(3001);
+// server.listen(3001);
+//  Express
+// const http = require('http');
+// const routes = require('./routes');
+const express = require('express');
+const app = express();
+app.use((req, res, next ) => {
+console.log("In the middleware");
+next();
+});
+app.use((req, res, next ) => {
+    console.log("In another middleware");
+    res.send('<h1>Hello from Express!</h1>');
+    })
+// const server = http.createServer(app);
+
+// server.listen(3000);
+app.listen(3000);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,4 @@
- module.exports = {
+//  module.exports = {
 //     handler :  requestHandler;
 //     someText : Some text;
 // }
