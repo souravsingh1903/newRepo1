5f38e9d2510542cf906ecc64004dd4cef60493c4
souravsingh1903 <souravsingh1903@gmail.com>
2024-02-01 22:57:46 +0530

    models done

--- controllers/product.js
+++ controllers/product.js
@@ -1,17 +1,22 @@
+
+const Product = require('../models/product');
 const path = require('path');
- const rootDir = require('../util/path');
- 
+const rootDir = require('../util/path');
+
  exports.getAddProduct = (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views','add-product.html'));
   }
 
 
   exports.postAddProducts = (req, res, next) => {
-    console.log(req.body);    
+    const product = new Product(req.body.title); 
+    product.save();  
     res.redirect("/");
   }
 
   exports.getProducts =  ( req, res, next) => {
+    const products = Product.fetchAll()
+    console.log(products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 }
 
---
+++ models/product.js
@@ -0,0 +1,16 @@
+const product = [];
+
+module.exports = class Product {
+    
+    constructor(title){
+        this.title = title;
+    }
+
+    save()  {
+        product.push(this);
+   }
+
+   static fetchAll(){
+    return product;
+   }
+}
